6dc1f4ac12c0e14e22a01c28b94aec2d3b11ba87
Andrey Koshechkin <andrey.koshechkin999@gmail.com>
2024-05-20 18:51:06 +0300

    init

--- src/main/java/com/example/viewtitle/view/CommonServiceView.java
+++ src/main/java/com/example/viewtitle/view/CommonServiceView.java
@@ -4,6 +4,7 @@ import com.example.viewtitle.view.titileViewV1.TitleBuilderImpl;
 import com.example.viewtitle.view.titileViewV2.TitleBuilderV2;
 import com.example.viewtitle.view.titileViewV2.TitleBuilderV2Impl;
 import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -17,8 +18,12 @@ public class CommonServiceView extends VerticalLayout {
     private TitleBuilderV2Impl titleBuilderV2 =  new TitleBuilderV2Impl();
     private Html titleV1;
     private Html titleV2;
+    private Button button = new Button("CRM");
 
     public CommonServiceView() {
+
+
+
         OrganizationData organizationData = new OrganizationData();
 
         organizationData.setName("ООО Совкомбанк");
@@ -31,7 +36,18 @@ public class CommonServiceView extends VerticalLayout {
         titleV1 = new Html(titleBuilderV1.commonServicesViewTitle(organizationData, BigDecimal.valueOf(11), BigDecimal.valueOf(151)));
         titleV2 = new Html(titleBuilderV2.commonServicesViewTitle(organizationData, BigDecimal.valueOf(11), BigDecimal.valueOf(151)));
 
-        add(titleV1, titleV2);
+        button.addClickListener(event -> {
+            getUI().ifPresent(ui -> ui.getPage().open("https://www.google.com", "_blank"));
+        });
+        add(titleV1, titleV2, button);
 
     }
 }
+/**
+ *
+ * _self: Открывает ресурс в том же фрейме или окне, в котором находится ссылка. Это значение по умолчанию.
+ * _parent: Открывает ресурс в родительском фрейме, если таковой существует. В противном случае ведет себя как _self.
+ * _top: Открывает ресурс в полном окне, отменяя все фреймы. Ведет себя как _self, если нет вложенных фреймов.
+ *
+ */
+
